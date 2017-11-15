9cebf9793f89675c77968fdb27aa658c7452d6a3
Karan Thakkar <karanjthakkar@gmail.com>
2017-11-15 13:53:44 +0530

    Remove npm auth verification code

--- src/lib/verify-auth.js
+++ src/lib/verify-auth.js
@@ -4,8 +4,4 @@ module.exports = (options, env) => {
   if (!options.githubToken) {
     throw new SemanticReleaseError('No github token specified.', 'ENOGHTOKEN');
   }
-
-  if (!(env.NPM_TOKEN || (env.NPM_OLD_TOKEN && env.NPM_EMAIL))) {
-    throw new SemanticReleaseError('No npm token specified.', 'ENONPMTOKEN');
-  }
 };
